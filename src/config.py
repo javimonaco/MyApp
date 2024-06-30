e3159085ed65dd68df1e21c4654a382312a25c67
javimonaco <109232005+javimonaco@users.noreply.github.com>
2024-06-30 11:28:49 -0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import boto3
+import os
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.environ.get('AWS_REGION')
+)
